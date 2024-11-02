a07b13b42c2f6847a3922aec41c23df89651b5f9
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2024-11-02 16:22:47 +0600

    first

--- src/first_java.java
+++ src/first_java.java
@@ -1,5 +1,6 @@
 public class first_java {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.printf("Amer soner bangla ami toami valo basi");
     }
 }
