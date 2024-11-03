3a9344b1e774b5f4df2290f8940f383fe93ff7c7
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2024-11-03 09:28:14 +0600

    third

---
+++ src/Operator_java.java
@@ -0,0 +1,89 @@
+public class Operator_java {
+    public static void main(String[] args) {
+        System.out.println("Operator in java programming");
+        //Operator: An operator is a symbol that performs operations on variable & value.
+        // An operator is a symbol that the compiler to per form a specific operation on operands.
+        // example:  a+b=c
+        // here:   = and + is operator.
+        // Operand: An operand in java is a value or variable upon which an operator acts.
+        // example:  a+b=c
+        // here:  a,b,c is operand.
+        // Aritmetic Operator :
+        /*
+        int a=10;
+        int b=20;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a/b;
+        int g=a%b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+         */
+        //increment operator
+        //pre-fix-
+        /*
+
+        int a=2;
+        int c= ++a;
+        System.out.println(c);
+         */
+        //increment operator
+        //post-fix-
+        /*
+        int a=2;
+        int c= a++;
+        System.out.println(c);
+         */
+        //Decrement operator
+        //pre-fix-
+        /*
+        int a=2;
+        int c= --a;
+        System.out.println(c);
+         */
+        //post-fix-
+        /*
+        int a=2;
+        int c= a--;
+        System.out.println(c);
+         */
+        //assignment operator:
+//        int a=10;
+        //here: '=' is assignment operator.
+
+        // Comparison Operator: its return true or false.
+//        int x=5;
+//        int y=2;
+//        System.out.println(x==y);
+//        System.out.println(x!=y);
+//        System.out.println(x<y);
+//        System.out.println(x>y);
+//        System.out.println(x<=y);
+//        System.out.println(x>=y);
+
+        // Logical Operator:
+//        int p=6;
+//        int q=5;
+//        int t=4;
+//        if ( p>q && p>t)
+//        {
+//            System.out.println("This value is true");
+//        }
+        //bitwise operator: it's return binary value 0 or 1.
+//        int a=9;
+//        int b=6;
+//        System.out.println(a&b);
+//        System.out.println(a|b);
+//        System.out.println(a^b);
+//        System.out.println(a<<b);
+//        System.out.println(a>>b);
+//        System.out.println(~a);
+
+
+
+    }
+}
